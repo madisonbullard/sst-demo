9d80754f082c15873b88ec0d04587a7a334cc8ca
Madison Bullard <madison.bullard@gmail.com>
2024-08-17 12:17:58 -0400

    Add biome and run p check

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,14 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "sst-demo-test",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
-      profile:
-        input.stage === "production" ? "sst-demo-production" : "sst-demo-dev",
-    };
-  },
-  async run() {},
+	app(input) {
+		return {
+			name: "sst-demo-test",
+			removal: input?.stage === "production" ? "retain" : "remove",
+			home: "aws",
+			profile:
+				input.stage === "production" ? "sst-demo-production" : "sst-demo-dev",
+		};
+	},
+	async run() {},
 });
