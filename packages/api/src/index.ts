e7c861a79103c795eef70b3b762a5f1cc2eea365
Madison Bullard <madison.bullard@gmail.com>
2024-08-17 21:29:21 -0400

    convert to json response

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,6 +3,6 @@ import { handle } from 'hono/aws-lambda'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/', (c) => c.json({ message: 'Hello Hono!' }))
 
 export const handler = handle(app)
