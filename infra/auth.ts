cc51f42e96a3f1fe50638ce7a37e698331af0cfd
Madison Bullard <madison.bullard@gmail.com>
2024-08-26 11:50:39 -0400

    Add packages/auth and github login infra

--- infra/auth.ts
+++ infra/auth.ts
@@ -1,11 +1,10 @@
-export const auth = new sst.aws.Auth("Auth", {
+const secrets = {
+	GithubClientID: new sst.Secret("GithubClientID"),
+	GithubClientSecret: new sst.Secret("GithubClientSecret"),
+};
+const auth = new sst.aws.Auth("Auth", {
 	authenticator: {
+		link: [secrets.GithubClientID, secrets.GithubClientSecret],
 		handler: "packages/auth/src/functions/index.handler",
-	},
-});
-
-export const authRouter = new sst.aws.Router("AuthRouter", {
-	routes: {
-		"/*": auth.url.apply((url) => url),
 	},
 });
---
+++ packages/auth/src/functions/index.ts
@@ -0,0 +1,32 @@
+import { Resource } from "sst";
+import { auth } from "sst/aws/auth";
+import { GithubAdapter } from "sst/auth/adapter";
+import { session } from "../utils/session.js";
+
+export const handler = auth.authorizer({
+	session,
+	providers: {
+		github: GithubAdapter({
+			clientID: Resource.GithubClientID.value,
+			mode: "oidc",
+		}),
+	},
+	callbacks: {
+		auth: {
+			async allowClient() {
+				return true;
+			},
+			async success(ctx, input) {
+				if (input.provider === "github") {
+					return ctx.session({
+						type: "user",
+						properties: {
+							email: input.tokenset.claims().email,
+						},
+					});
+				}
+				return new Response("Uh oh", { status: 400 });
+			},
+		},
+	},
+});
